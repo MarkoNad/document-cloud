b56afd8fd8738b0b948745635ff2848e9bc2571e
MarkoNad <marko.nad123@gmail.com>
2021-01-23 18:14:10 +0100

    Add basic auth.

--- src/main/java/hr/documentcloud/DocumentCloudApplication.java
+++ src/main/java/hr/documentcloud/DocumentCloudApplication.java
@@ -3,6 +3,8 @@ package hr.documentcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.concurrent.ExecutorService;
